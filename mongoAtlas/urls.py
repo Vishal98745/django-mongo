1e3ed888af6627e9891f0c70db0fc53214514686
Vishal Shrivastava <vishal71shrivastava@gmail.com>
2023-07-21 22:41:25 +0530

    changes in url file

--- mongoAtlas/urls.py
+++ mongoAtlas/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from mongoapp.views import Student
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',Student.as_view()),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
