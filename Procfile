e3a5d771171116fc9937981b55167827808e6780
Vishal Shrivastava <vishal71shrivastava@gmail.com>
2023-07-21 12:24:37 +0530

    changes done in setting file and include procfile and runtime file in project

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mongoAtlas.wsgi --log-file -
--- mongoAtlas/settings.py
+++ mongoAtlas/settings.py
@@ -11,10 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -80,12 +78,7 @@ WSGI_APPLICATION = 'mongoAtlas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
